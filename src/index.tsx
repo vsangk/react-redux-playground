8597cb0e1eb59e6a0293969cdebc22c04d42b424
Vince Sangkhae <vsangk@gmail.com>
2018-08-08 19:43:25 -0700

    Add redux

--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,26 @@
 import * as React from "react";
 import * as ReactDOM from "react-dom";
-import Hello from "./components/Hello";
+import Hello from "./containers/Hello";
 import "./index.css";
 import registerServiceWorker from "./registerServiceWorker";
+import { createStore } from "redux";
+import { enthusiasm } from "./reducers";
+import { StoreState } from "./types";
+import { Provider } from "react-redux";
+import { EnthusiasmAction } from "./actions";
+
+const store = createStore<StoreState, EnthusiasmAction, null, null>(
+  enthusiasm,
+  {
+    enthusiasmLevel: 1,
+    languageName: "TypeScript"
+  }
+);
 
 ReactDOM.render(
-  <Hello name="TypeScript" enthusiasmLevel={10} />,
+  <Provider store={store}>
+    <Hello />
+  </Provider>,
   document.getElementById("root") as HTMLElement
 );
 registerServiceWorker();
